ec88322d55a3e8da78b8f6d87b5a10be23412ca3
sarena91 <sarenagovender@gmail.com>
2021-05-18 22:06:35 +0200

    Added addTodo feature. Changed babel config

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,25 @@
 <template>
   <div id="app">
+   <Header/>
+   <AddTodo @add-todo="addTodo" />
    <Todos :todos="todos" @del-todo="deleteTodo" />
   </div>
 </template>
 
 <script>
+// import "core-js/stable";
+// import "regenerator-runtime/runtime";
+
+import Header from './components/layout/Header';
 import Todos from './components/Todos';
+import AddTodo from './components/AddTodo';
 
 export default {
   name: 'App',
   components: {
-    Todos
+    Header,
+    Todos,
+    AddTodo
   },
   data(){
     return{
@@ -41,6 +50,9 @@ export default {
   methods: {
     deleteTodo(id){
       this.todos = this.todos.filter(todo => todo.id !== id);
+    },
+    addTodo(newTodo){
+      this.todos = [...this.todos, newTodo];
     }
   }
 }
@@ -56,5 +68,18 @@ export default {
 body {
   font-family: Arial, Helvetica, sans-serif;
   line-height: 1.4;
+}
+
+.btn {
+  display: inline-block;
+  border: none;
+  background: #555;
+  color: #fff;
+  padding: 7px 20px;
+  cursor: pointer;
+}
+
+.btn:hover {
+  background: #666;
 }
 </style>
